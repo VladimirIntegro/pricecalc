f7761315abf833b1bff5140e1dec460937ce4d9c
VladimirIntegro <integrostudio@gmail.com>
2017-02-05 02:20:39 +0300

    First commit

---
+++ assets/tpl/js/vmz.price.calc/vmz.price.calc.js
@@ -0,0 +1,199 @@
+/*
+ * Price calculator
+ * http://github.com/VladimirIntegro/pricecalc
+ * Copyright (c) 2016 -	Vladimir Zhitkov
+ * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
+ * Version: 0.0.0-dev
+ */
+
+// Test calculator values for the pattern and return false if the value matches the pattern
+function TestCalcFormValueForPattern(aVal, aPattern, aFieldName)
+{
+ 	var retErr = false;
+ 	if(aVal != '') {
+		var pattern = aPattern;
+		if(pattern.test(aVal)) {
+	  	$(aFieldName).css({'border' : '1px solid #acadad'});
+		} else {
+	  		$(aFieldName).css({'border' : '1px solid red'});
+	  		retErr = true;
+		}
+  	} else {
+		$(aFieldName).css({'border' : '1px solid red'});
+		retErr = true;
+  	}
+  	return retErr;
+}
+
+$("#calc_contacts_phone").inputmask("mask", {"mask": "+7 (999) 999-99-99", clearMaskOnLostFocus: false});
+
+// Main page calculator
+$('#kalk_form').submit(function() {
+    var manuf = $("#calc_manuf").val();
+    var square = $("#calc_square").val();
+    var corner = $("#calc_corner").val();
+    var trub = $("#calc_trub").val();
+    var lustr = $("#calc_lustr").val();
+    var svetil = $("#calc_svetil").val();
+    var plintus = $("#calc_plintus").val();
+	var url = $("#calc_url").val();
+    var pageid = gPageId;
+
+    var err = false;
+    // Check name
+    if(manuf != '') {
+        //var pattern = /^[a-z0-9а-яА-ЯёЁ\s\.\-]+$/i;
+        //if(pattern.test(name)){
+		if(0 < manuf && manuf < 5) {
+            $('#calc_manuf').css({'border' : '1px solid #acadad'});
+        } else {
+            $('#calc_manuf').css({'border' : '1px solid red'});
+            err = true;
+        }
+    } else {
+        $('#calc_manuf').css({'border' : '1px solid red'});
+        err = true;
+    }
+
+	// Check square
+	err = TestCalcFormValueForPattern(square, /^[0-9\.]+$/i, '#calc_square');
+	// Check corner
+	err = TestCalcFormValueForPattern(corner, /^[0-9]+$/i, '#calc_corner');
+	// Check trub
+	err = TestCalcFormValueForPattern(trub, /^[0-9]+$/i, '#calc_trub');
+	// Check lustr
+	err = TestCalcFormValueForPattern(lustr, /^[0-9]+$/i, '#calc_lustr');
+	// Check svetil
+	err = TestCalcFormValueForPattern(svetil, /^[0-9]+$/i, '#calc_svetil');
+	// Check plintus
+	err = TestCalcFormValueForPattern(plintus, /^[0-9\.]+$/i, '#calc_plintus');
+
+    if(err === false){
+        var contactFormData = new FormData();
+        contactFormData.append('manuf', manuf);
+        contactFormData.append('square', square);
+        contactFormData.append('corner', corner);
+        contactFormData.append('trub', trub);
+        contactFormData.append('lustr', lustr);
+        contactFormData.append('svetil', svetil);
+        contactFormData.append('plintus', plintus);
+        contactFormData.append('url', url);
+        contactFormData.append('id', pageid);
+
+        $.ajax({
+          type: "POST",  
+          url: gSendToCalcUrl,  
+          data: contactFormData,
+          processData: false,
+          contentType: false,
+          success:  function(data) { 
+			  //console.log(data);
+			  $("#calc_conts_send_manuf").val(data['manuf']);
+			  $("#calc_conts_send_square").val(data['square']);
+			  $("#calc_conts_send_corner").val(data['corner']);
+			  $("#calc_conts_send_trub").val(data['trub']);
+			  $("#calc_conts_send_lustr").val(data['lustr']);
+			  $("#calc_conts_send_svetil").val(data['svetil']);
+			  $("#calc_conts_send_plintus").val(data['plintus']);
+			  $("#calc_conts_send_sum").val(data['sum']);
+			  $('#kalk_form_results .kalk_form_results_sum').text(data['remontPrice'] + ' рублей');
+			  $('#kalk_form').hide(200);
+			  $('#kalk_form_results').show(300);
+			  $('.kalk_form_send_contacts_cont').show(300);
+          },
+          error:  function(xhr, status, error) {
+			  var err = eval("(" + xhr.responseText + ")");
+			  if(err.error == "FILE UPLOAD ERROR") {
+				$('#kalk_form_results').hide();
+				$('#kalk_form_results .kalk_form_results_sum').text('');
+				$('.kalk_form_send_contacts_cont').hide();
+				$('#kalk_form').show(300);
+			  }
+		  }
+        });  
+    }
+    return false;
+});
+
+// Main page calculator send contacts form
+$('#sendCalcContactsBottomForm').submit(function() {
+    var manuf = $("#calc_conts_send_manuf").val();
+    var square = $("#calc_conts_send_square").val();
+    var corner = $("#calc_conts_send_corner").val();
+    var trub = $("#calc_conts_send_trub").val();
+    var lustr = $("#calc_conts_send_lustr").val();
+    var svetil = $("#calc_conts_send_svetil").val();
+    var plintus = $("#calc_conts_send_plintus").val();
+    var sum = $("#calc_conts_send_sum").val();
+    var name = $("#calc_contacts_name").val();
+    var phone = $("#calc_contacts_phone").val();
+	var url = $("#calc_contacts_url").val();
+    var pageid = gPageId;
+
+    var err = false;
+	// Check name
+    if(name != '') {
+        var pattern = /^[a-z0-9а-яА-ЯёЁ\s\.\-]+$/i;
+        if(pattern.test(name)){
+            $('#calc_contacts_name').css({'border' : '1px solid #8a8f93'});
+        } else {
+            $('#calc_contacts_name').css({'border' : '1px solid red'});
+            err=true;
+        }
+    } else {
+        $('#calc_contacts_name').css({'border' : '1px solid red'});
+        err=true;
+    }
+    // Check phone
+    if(phone != '') {
+        var pattern = /^[0-9\s\.\-\(\)\+]+$/i;
+        if(pattern.test(phone)){
+            $('#calc_contacts_phone').css({'border' : '1px solid #8a8f93'});
+        } else {
+            $('#calc_contacts_phone').css({'border' : '1px solid red'});
+            err=true;
+        }
+    } else {
+        $('#calc_contacts_phone').css({'border' : '1px solid red'});
+        err=true;
+    }
+
+    if(err === false){
+        var contactFormData = new FormData();
+        contactFormData.append('manuf', manuf);
+        contactFormData.append('square', square);
+        contactFormData.append('corner', corner);
+        contactFormData.append('trub', trub);
+        contactFormData.append('lustr', lustr);
+        contactFormData.append('svetil', svetil);
+        contactFormData.append('plintus', plintus);
+        contactFormData.append('sum', sum);
+        contactFormData.append('name', name);
+        contactFormData.append('phone', phone);
+        contactFormData.append('url', url);
+        contactFormData.append('id', pageid);
+
+        $.ajax({
+          type: "POST",  
+          url: gSendCalcResultsUrl,  
+          data: contactFormData,
+          processData: false,
+          contentType: false,
+          success:  function(data) { 
+			  //console.log(data);
+			  $('#kalk_form_send_contacts-bot').css({'color' : '#ffa039', 'font-weight' : 'bold'});
+			  $('#kalk_form_send_contacts-bot').text("Заявка отправлена!");
+			  $('#kalk_form_send_contacts-bot').show(300);
+          },
+          error:  function(xhr, status, error) {
+			  var err = eval("(" + xhr.responseText + ")");
+			  if(err.error == "FILE UPLOAD ERROR") {
+				$('#kalk_form_send_contacts-bot').css({'color' : 'red', 'font-weight' : 'bold'});
+			  	$('#kalk_form_send_contacts-bot').text("Не удалось отправить. Повторите попытку!");
+			  	$('#kalk_form_send_contacts-bot').show(300);
+			  }
+		  }
+        });  
+    }
+    return false;
+});
---
+++ assets/tpl/js/vmz.price.calc/vmz.price.calc.main.tpl
@@ -0,0 +1,80 @@
+	<div class="kalk_form_cont_back">
+		<div id="kalk_form_cont">
+			<h1>КАЛЬКУЛЯТОР СТОИМОСТИ</h1>
+			<p class="error"></p>
+			<form method="post" id="kalk_form">
+				<table>
+					<tbody>
+						<tr>
+						<td class="kalk_form_table_lf"><label>Производитель</label></td>
+						<td><select id="calc_manuf" name="calc_manuf" class="formField_kalk">
+						<option value="1">Бельгия («Polyplast», ПВХ)</option>
+						<option value="2">Германия («Descor», Ткань)</option>
+						<option value="3">Германия («Pongs», ПВХ)</option>
+						<option value="4">Россия (Казань, ПВХ)</option>
+						</select></td>
+						</tr>
+						<tr>
+						<td class="kalk_form_table_lf"><label>Площадь помещения, м<sup>2</sup> <p>(разделителем должна быть точка ".") *</p></label></td>
+						<td><input id="calc_square" class="formField_kalk" name="calc_square" value="" type="text"></td>
+						</tr>
+						<tr>
+						<td class="kalk_form_table_lf"><label>Количество углов <span>(до 4 - бесплатно) *</span></label></td>
+						<td><input id="calc_corner" class="formField_kalk" name="calc_corner" value="" type="text"></td>
+						</tr>
+						<tr>
+						<td class="kalk_form_table_lf"><label>Количество трубопроводов *</label></td>
+						<td><input id="calc_trub" class="formField_kalk" name="calc_trub" value="" type="text"></td>
+						</tr>
+						<tr>
+						<td class="kalk_form_table_lf"><label>Количество люстр *</label></td>
+						<td><input id="calc_lustr" class="formField_kalk" name="calc_lustr" value="" type="text"></td>
+						</tr><tr>
+						<td class="kalk_form_table_lf"><label>Количество точечных светильников *</label></td>
+						<td><input id="calc_svetil" class="formField_kalk" name="calc_svetil" value="" type="text"></td>
+						</tr>
+						<tr>
+						<td class="kalk_form_table_lf"><label>Плинтус (м.п.) *</label></td>
+						<td><input id="calc_plintus" class="formField_kalk" name="calc_plintus" value="" type="text"></td>
+						</tr>
+					</tbody>
+				</table>
+				<div class="formbutton_kalk_cont">
+					<input class="formbutton_kalk" value="Рассчитать стоимость" type="submit">
+					<p class="hidurlfield">Не заполняйте это поле: <input type="text" id="calc_url" name="url" placeholder="Не заполняйте это поле!" value=""></p>
+				</div>
+			</form>
+			<table id="kalk_form_results">
+				<tbody><tr><td>Полная стоимость установки потолков: </td><td class="kalk_form_results_sum"></td></tr></tbody>		
+			</table>
+			<div class="kalk_form_send_contacts_cont">
+				<div class="kalk_form_send_contacts">
+					<div class="obratniy-zvonok-title">
+						<div id="kalk_form_send_contacts-title">Вызов замерщика</div>
+						<p id="kalk_form_send_contacts-bot"></p>
+						<div class="obratniy-zvonok-content">
+							<form id="sendCalcContactsBottomForm" method="post">
+								<fieldset>
+									<input type="hidden" id="calc_conts_send_manuf" name="calc_conts_send_manuf" value="">
+									<input type="hidden" id="calc_conts_send_square" name="calc_conts_send_square" value="">
+									<input type="hidden" id="calc_conts_send_corner" name="calc_conts_send_corner" value="">
+									<input type="hidden" id="calc_conts_send_trub" name="calc_conts_send_trub" value="">
+									<input type="hidden" id="calc_conts_send_lustr" name="calc_conts_send_lustr" value="">
+									<input type="hidden" id="calc_conts_send_svetil" name="calc_conts_send_svetil" value="">
+									<input type="hidden" id="calc_conts_send_plintus" name="calc_conts_send_plintus" value="">
+									<input type="hidden" id="calc_conts_send_sum" name="calc_conts_send_sum" value="">
+									<input class="formfield" id="calc_contacts_name" name="calc_contacts_name" minlength="2" value="[[!+fi.calc_contacts_name]]" required="required" type="text" placeholder="Ваше имя">
+									<span class="error">[[!+fi.error.calc_contacts_name]]</span>
+									<input class="formfield" id="calc_contacts_phone" name="calc_contacts_phone" value="[[!+fi.calc_contacts_phone_NA_format]]" required="required" type="text" placeholder="Ваш телефон">
+									<span class="error">[[!+fi.error.calc_contacts_phone_NA_format]]</span>
+									<input class="formbutton" value="Вызвать замерщика" type="submit">
+									<p class="hidurlfield">Не заполняйте это поле: <input type="text" id="calc_contacts_url" name="url" placeholder="Не заполняйте это поле!" value=""></p>
+								</fieldset>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="wrap-40px"></div>
+			</div>
+		</div>
+	</div>
